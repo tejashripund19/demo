c76c5cdf9b93727eb5272e0500ccdf94f5644469
Tejashri Pund <ptejashri19@gmail.com>
2020-05-14 16:54:06 +0530

    installed and added nodemon

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use("/", userRouter);
 module.exports = app;
 
 //start server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app running on ${port}..`);
 });
